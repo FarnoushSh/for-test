a62f1fff4af20e7b1b59fee8418bbabb75e22991
FarnoushSh <haminsi63@gmail.com>
2018-12-05 09:40:14 +0100

    DataLab

--- DataLab.py
+++ DataLab.py
@@ -30,32 +30,25 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import ExtraTreesClassifier
 
 
-
+# Loading the data
 train_data=pd.read_csv('TrainDataApo.csv',sep=';', engine='python')
 test_data=pd.read_csv('TestDataApo.csv', sep=';', engine='python')
+
+# To replace the white space in headers with underline
 train_data.columns = train_data.columns.str.replace(' ', '_')
 test_data.columns = test_data.columns.str.replace(' ', '_')
-# train_data.rename(columns={ 'Schulabschlu�' : 'Schulabschluss'}, inplace=True)
-# test_data.rename(columns={ 'Schulabschlu�' : 'Schulabschluss'}, inplace=True)
 print(train_data.columns)
 
+# Looking for object data to see how to catogorize them
 obj_data = train_data.select_dtypes(include=['object']).copy()
 obj_col=obj_data.columns
 for col in obj_col:
     print(obj_data.groupby([col]).size())
 
-# def change_string(x):
-#     return x.replace('Gr�nder',"Greunder").replace('Selbst�ndig','Selbsteandig')
-#
-# train_data['Art_der_Anstellung'] = train_data['Art_der_Anstellung'].map(lambda x: change_string(x))
-# test_data['Art_der_Anstellung'] = test_data['Art_der_Anstellung'].map(lambda x: change_string(x))
-# test_data.groupby('Art_der_Anstellung').size()
-
-
 
 print(train_data.groupby('Zielvariable').mean())
 
-#
+
 # # Data Visualization
 pd.crosstab(train_data.Monat,train_data.Zielvariable).plot(kind='bar')
 plt.title('Zielvariable & Monat')
@@ -127,8 +120,7 @@ plt.figure(figsize=(5,5))
 sns.boxplot(x='Zielvariable',y='Anzahl_Kontakte_letzte_Kampagne',data=train_data)
 plt.show()
 
-# OUTLIERS
-
+# Outliers ?!
 sns.FacetGrid(train_data, hue="Zielvariable", height=5) \
    .map(plt.scatter, "Dauer", "Alter") \
    .add_legend()
@@ -144,56 +136,54 @@ sns.FacetGrid(train_data, hue="Zielvariable", height=5) \
    .add_legend()
 plt.show()
 
-
+# Removing Outliers
 trolls_tr = train_data[(train_data.Kontostand >= 50000) | (train_data.Dauer >=2500)|(train_data.Anzahl_Kontakte_letzte_Kampagne >= 50)].index
 train_data= train_data.drop(list(trolls_tr), axis=0)
 trolls_ts = test_data[(test_data.Kontostand >= 50000) | (test_data.Dauer >=2500)|(train_data.Anzahl_Kontakte_letzte_Kampagne >= 50)].index
 test_data= test_data.drop(list(trolls_ts), axis=0)
 
 
-# # MissingValues
-#
+# MissingValues
 sns.heatmap(train_data.isnull(),yticklabels=False)
 plt.show()
 
 
-#After some examination, having or not having the Tag... variable does not change the result and so I drop it.
+#After some examination, having or not having the Tag... elmiable does not change the result and so I drop it.
 train_data.drop('Tage_seit_letzter_Kampagne',axis=1,inplace=True)
 
 
 # encoding target values manually.
-cleanup_nums = {'Zielvariable':{'nein':0,'ja':1}}
-train_data.replace(cleanup_nums, inplace=True)
+cat_rep = {'Zielvariable':{'nein':0,'ja':1}}
+train_data.replace(cat_rep, inplace=True)
 
-# change the continues variable kontostand into three categories (negative, positive and null)
+# change the continues elmiable kontostand into three categories (negative, positive and null)
 train_data['Kontostand'] = ['Negative' if x < 0 else 'Null' if x == 0 else 'positive' if x > 0 else 'NaN' for x in train_data['Kontostand']]
 test_data['Kontostand'] = ['Negative' if x < 0 else 'Null' if x == 0 else 'positive' if x > 0 else 'NaN' for x in test_data['Kontostand']]
-#
 
 
 #  Encoding categorical data using get_dummies
-cat_vars_list=['Kontostand','Familienstand','Haus','Kredit','Ausfall_Kredit','Kontaktart',
+cat_elms_list=['Kontostand','Familienstand','Haus','Kredit','Ausfall_Kredit','Kontaktart',
           'Schulabschluß', 'Ergebnis_letzte_Kampagne','Art_der_Anstellung','Monat']
-for var in cat_vars_list:
-    cat_var_train='var'+'_'+ var
-    cat_var_train = pd.get_dummies(train_data[var], prefix=var)
-    cat_var_test= 'var' + '_' + var
-    cat_var_test = pd.get_dummies(test_data[var], prefix=var)
-    data1 = train_data.join(cat_var_train)
-    data2 = test_data.join(cat_var_test)
+for elm in cat_elms_list:
+    cat_elm_train='elm'+'_'+ elm
+    cat_elm_train = pd.get_dummies(train_data[elm], prefix=elm)
+    cat_elm_test= 'elm' + '_' + elm
+    cat_elm_test = pd.get_dummies(test_data[elm], prefix=elm)
+    data1 = train_data.join(cat_elm_train)
+    data2 = test_data.join(cat_elm_test)
     train_data = data1
     test_data = data2
 
-data_vars_train = train_data.columns.values.tolist()
-data_vars_test = test_data.columns.values.tolist()
-keep_train = [i for i in data_vars_train if i not in cat_vars_list]
+data_elms_train = train_data.columns.values.tolist()
+data_elms_test = test_data.columns.values.tolist()
+keep_train = [i for i in data_elms_train if i not in cat_elms_list]
 Train_dt = train_data[keep_train]
-keep_test=[i for i in data_vars_test if i not in cat_vars_list]
+keep_test=[i for i in data_elms_test if i not in cat_elms_list]
 Test_dt=test_data[keep_test]
 print(Train_dt.columns)
 print(Test_dt.columns)
 
-# droping some unuseful variables
+# droping some unuseful elmiables
 Train_dt=Train_dt.drop(['Tag','Stammnummer','Anruf-ID','Geschlecht'],axis=1)
 Test_dt=Test_dt.drop(['Tag','Stammnummer','Anruf-ID','Geschlecht','Zielvariable'],axis=1)
 
@@ -222,52 +212,44 @@ x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 smt = SMOTE(random_state=0)
 columns = x_train.columns
 smt_data_X,smt_data_y = smt.fit_sample(x_train, y_train.values.ravel())
-smt_data_X = pd.DataFrame(data=smt_data_X,columns=columns )
-smt_data_y= pd.DataFrame(data=smt_data_y,columns=['Zielvariable'])
-print("length of oversampled data is ",len(smt_data_X))
-print("Number of no subscription in oversampled data",len(smt_data_y[smt_data_y['Zielvariable']==0]))
-print("Number of subscription",len(smt_data_y[smt_data_y['Zielvariable']==1]))
-print("Proportion of no subscription data in oversampled data is ",len(smt_data_y[smt_data_y['Zielvariable']==0])/len(smt_data_X))
-print("Proportion of subscription data in oversampled data is ",len(smt_data_y[smt_data_y['Zielvariable']==1])/len(smt_data_X))
-#
+smt_data_X = pd.DataFrame(data = smt_data_X,columns=columns )
+smt_data_y = pd.DataFrame(data = smt_data_y,columns=['Zielvariable'])
+print("Length of oversampled data: ",len(smt_data_X))
+print("Ratio of zero target value in oversampled data : ",len(smt_data_y[smt_data_y['Zielvariable'] == 0])/len(smt_data_X))
+print("Ratio of one target value in oversampled data :  ",len(smt_data_y[smt_data_y['Zielvariable'] == 1])/len(smt_data_X))
 
-#
 
-# #
-# # # Feature Optimization
+# Feature Optimization
 X = smt_data_X
 y = smt_data_y['Zielvariable']
 
-#
 
-#
 # Data Training (Logistic Regression. Random Forest Classsifier, Decision Tree Classifier)
 selected_x = smt_data_X[Features_name]
 X_train, X_test, y_train, y_test = train_test_split(selected_x, y, test_size=0.3, random_state=0)
+
 logreg = LogisticRegression()
 predicted = cross_val_predict(logreg, selected_x, y, cv=10)
 logreg.fit(X_train, y_train)
-#
 y_pred_lr = logreg.predict(X_test)
-print('Accuracy of logistic regression classifier on test set: {:.2f}'.format(logreg.score(X_test,y_test)))
-print('Logistic-regression (cross_validation):',metrics.accuracy_score(y, predicted))
+print('Accuracy of LogisticRegressionClassifier on test set: {:.2f}'.format(logreg.score(X_test,y_test)))
+print('LogisticRegression (cross_validation):',metrics.accuracy_score(y, predicted))
 
 confusion_matrix = confusion_matrix(y_pred_lr,y_test)
 print(confusion_matrix)
 
 print(classification_report(y_pred_lr,y_test))
-#
+
+
 dtc = DecisionTreeClassifier()
 dtc.fit(X_train, y_train)
-
 y_pred = dtc.predict(X_test)
-print('Accuracy of Decision Tree classifier on test set: {:.2f}'.format(dtc.score(X_test, y_test)))
+print('Accuracy of DecisionTreeClassifier on test set: {:.2f}'.format(dtc.score(X_test, y_test)))
 
 rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
-
 y_pred = rf.predict(X_test)
-print('Accuracy of Random Forest classifier on test set: {:.2f}'.format(rf.score(X_test, y_test)))
+print('Accuracy of RandomForestClassifier on test set: {:.2f}'.format(rf.score(X_test, y_test)))
 
 
 # Comparing the results
@@ -278,18 +260,17 @@ names = []
 Z = [DecisionTreeClassifier(), LogisticRegression(), RandomForestClassifier()]
 name = ["DecisionTreeClassifier", "LogisticRegression", "RandomForestClassifier"]
 
-seed = 10
 for i in range(0, len(Z)):
     model = Z[i]
-    kfold = model_selection.KFold(n_splits=10, random_state=seed)
-    cv_results = model_selection.cross_val_score(model, selected_x, y, cv=kfold, scoring='accuracy')
+    k_fold = model_selection.KFold(n_splits = 10, random_state = 10)
+    cv_results = model_selection.cross_val_score(model, selected_x, y, cv = k_fold, scoring = 'accuracy')
     results.append(cv_results)
     names.append(name)
     msg = "%s: %f (%f)" % (name[i], cv_results.mean(), cv_results.std())
     print(msg)
 
 fig = plt.figure(figsize=(10, 10))
-fig.suptitle('Algorithm Comparison')
+fig.suptitle('Comparing the Algorithm')
 ax = fig.add_subplot(111)
 plt.boxplot(results)
 ax.set_xticklabels(name)
@@ -337,11 +318,11 @@ for m in models:
     model.fit(X_train, y_train)
     y_pred_lr= model.predict(X_test)
 
-    fpr, tpr, thresholds = metrics.roc_curve(y_test, model.predict_proba(X_test)[:, 1])
+    fp, tp, thresholds = metrics.roc_curve(y_test, model.predict_proba(X_test)[:, 1])
 
     auc = metrics.roc_auc_score(y_test, model.predict(X_test))
 
-    plt.plot(fpr, tpr, label='%s ROC (area = %0.2f)' % (m['label'], auc))
+    plt.plot(fp, tp, label='%s ROC (area = %0.2f)' % (m['label'], auc))
 
 plt.plot([0, 1], [0, 1], 'r--')
 plt.xlim([0.0, 1.0])
